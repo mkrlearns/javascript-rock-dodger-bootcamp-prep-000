5fefd286d17e664495d184fa7da891c232f571cf
ryan m <contact@ryanmeek.com>
2019-01-21 19:36:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -94,11 +94,11 @@ function createRock(x) {
      */
    function moveRock() {
      rock.style.top = `${top += 2}px`;
- 
+
      if (checkCollision(rock)) {
        return endGame()
      }
- 
+
      if (top < GAME_HEIGHT) {
        window.requestAnimationFrame(moveRock)
      } else {
